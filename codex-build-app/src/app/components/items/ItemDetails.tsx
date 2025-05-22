fd1c0221a1fd1adc82bb3c31db88979749ddc683
[MISUMI| Hieu HoMinh <164456917+misumi-hieu-hominh-01@users.noreply.github.com>
2025-05-22 11:13:13 +0900

    Add Modal and ItemDetails placeholder components (#13)

---
+++ codex-build-app/src/app/components/items/ItemDetails.tsx
@@ -0,0 +1,3 @@
+export function ItemDetails() {
+  return <div>Item Details Placeholder</div>;
+}
---
+++ codex-build-app/src/app/components/ui/Modal.tsx
@@ -0,0 +1,5 @@
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function Modal(props: ModalProps) {
+  return <div {...props}>Modal Placeholder</div>;
+}
