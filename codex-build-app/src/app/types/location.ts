e48eb300b0884f21687b96c64ebfd74b8de901ef
[MISUMI| Hieu HoMinh <164456917+misumi-hieu-hominh-01@users.noreply.github.com>
2025-05-22 11:28:20 +0900

    Add StorageLocation interface

--- codex-build-app/src/app/types/location.ts
+++ codex-build-app/src/app/types/location.ts
@@ -2,3 +2,10 @@ export interface Location {
   id: string;
   name?: string;
 }
+
+export interface StorageLocation {
+  _id: string;
+  name: string;
+  description?: string;
+  imageUrl?: string;
+}
