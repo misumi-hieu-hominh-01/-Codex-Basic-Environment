b5176233168008ee4c5d8544c005dc1f7a24a6b2
[MISUMI| Hieu HoMinh <164456917+misumi-hieu-hominh-01@users.noreply.github.com>
2025-05-22 12:12:41 +0900

    feat(ui): implement Input component

--- codex-build-app/src/app/components/ui/Input.tsx
+++ codex-build-app/src/app/components/ui/Input.tsx
@@ -1,5 +1,16 @@
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+import React from "react";
+import styles from "./Input.module.css";
 
-export function Input(props: InputProps) {
-  return <input {...props} />;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  /** Additional class names to apply */
+  className?: string;
 }
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    const classes = [styles.input, className].filter(Boolean).join(" ");
+    return <input ref={ref} className={classes} {...props} />;
+  }
+);
+
+Input.displayName = "Input";
